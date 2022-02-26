7a526c8f47549b45968993bba5cda6c9c71b8616
Eusebio <eusloprob@alum.us.es>
2022-02-26 21:07:50 +0100

    [MOD] Instalación de axios y prueba de conexión

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyectoAngular';
+  getAxiosData(){
+    var api = "http://localhost:8091/user/data";
+    axios.get(api)
+      .then(function (response) {
+        console.log(response);
+      });
+  }
 }
