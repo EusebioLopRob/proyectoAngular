ce4eb0fdf7df59bfa42164ec7eabaa3e5c0ca88d
Eusebio <eusloprob@alum.us.es>
2022-02-27 18:13:24 +0100

    [MOD] Readaptación del entorno angular

--- 1-frontend/karma.conf.js
+++ 1-frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/app-curso'),
+      dir: require('path').join(__dirname, './coverage/1-fronend'),
       subdir: '.',
       reporters: [
         { type: 'html' },
