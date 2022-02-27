aab6d95d1084584d293966c433efa516db97a14b
Eusebio <eusloprob@alum.us.es>
2022-02-27 21:58:18 +0100

    [MOD] Primer esbozo del componente de lista de usuarios

--- 1-frontend/src/app/app.module.ts
+++ 1-frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PruebaComponent } from './components/prueba/prueba.component';
 import { Prueba2Component } from './components/prueba2/prueba2.component';
 import { Prueba3Component } from './components/prueba3/prueba3.component';
+import { UserlistComponent } from './components/userlist/userlist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PruebaComponent,
     Prueba2Component,
-    Prueba3Component
+    Prueba3Component,
+    UserlistComponent
   ],
   imports: [
     BrowserModule,
